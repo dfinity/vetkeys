29aed77cb0bb627b05316ca8c4453d848ad3e2fa
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-05-02 17:00:52 +0200

    ...

--- examples/password_manager/frontend/eslint.config.mjs
+++ examples/password_manager/frontend/eslint.config.mjs
@@ -2,9 +2,32 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import svelte from "eslint-plugin-svelte";
+import svelteConfig from "./svelte.config.js";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  { ignores: ["dist/", "babel.config.js", "src/declarations", "vite.config.ts"] }
+  ...svelte.configs.recommended,
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        extraFileExtensions: [".svelte"],
+        parser: tseslint.parser,
+        svelteConfig,
+      },
+    },
+  },
+  {
+    ignores: [
+      "dist/",
+      "src/declarations",
+      "*.config.js",
+      "*.config.cjs",
+      "*.config.mjs",
+      "*.config.ts",
+    ],
+  }
 );
