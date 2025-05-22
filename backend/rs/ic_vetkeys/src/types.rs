255ea5d931689b1517cfc36ac22fd84cd202e893
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-05-22 09:17:46 +0200

    access rights

--- backend/rs/ic_vetkeys/src/types.rs
+++ backend/rs/ic_vetkeys/src/types.rs
@@ -15,9 +15,9 @@ pub type MapKey = Blob<32>;
 pub type TransportKey = ByteBuf;
 pub type EncryptedMapValue = ByteBuf;
 
-/// The system enforces access control based on [`AccessRights`], allowing fine-grained control over key usage:
+/// Access rights of a user to a vetKey in [`crate::key_manager::KeyManager`] and/or an encrypted map in [`crate::encrypted_maps::EncryptedMaps`].
 ///
-/// - **Read**: User can retrieve the encrypted key.
+/// - **Read**: 
 /// - **ReadWrite**: User can update the key.
 /// - **ReadWriteManage**: User can view/share/revoke access.
 #[repr(u8)]
@@ -36,8 +36,11 @@ pub type EncryptedMapValue = ByteBuf;
     strum_macros::EnumIter,
 )]
 pub enum AccessRights {
+    /// User can retrieve the vetKey or encrypted map.
     Read = 0,
+    /// User can update values in the encrypted map.
     ReadWrite = 1,
+    /// User can view/share/revoke access to the vetKey or encrypted map.
     ReadWriteManage = 2,
 }
 
@@ -99,10 +102,15 @@ pub trait AccessControl:
     + strum::IntoEnumIterator
     + Storable
 {
+    /// Returns if the user can read the vetKey or encrypted map.
     fn can_read(&self) -> bool;
+    /// Returns if the user can write to the vetKey or encrypted map.
     fn can_write(&self) -> bool;
+    /// Returns if the user can view the access rights to the vetKey or encrypted map.
     fn can_get_user_rights(&self) -> bool;
+    /// Returns if the user can modify the access rights to the vetKey or encrypted map.
     fn can_set_user_rights(&self) -> bool;
+    /// Returns the access rights of the owner of the vetKey or encrypted map.
     fn owner_rights() -> Self;
 }
 
