9f072dce194bb3696dcc2a8b05d0303ce86074b5
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-05-30 18:43:19 +0200

    polyfilling crypto is not needed anymore in node

--- frontend/ic_vetkeys/test/setup.ts
+++ frontend/ic_vetkeys/test/setup.ts
@@ -1,12 +1,6 @@
 import { beforeAll } from "vitest";
 import indexeddb from "fake-indexeddb";
-import crypto from "node:crypto";
 
 beforeAll(() => {
-    Object.defineProperty(globalThis, "crypto", {
-        value: crypto.webcrypto,
-        writable: true,
-    });
-
     globalThis.indexedDB = indexeddb;
 });
