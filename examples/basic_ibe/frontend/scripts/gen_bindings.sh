61f824841c0e714256704a9e188448425cf4f00a
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-25 12:16:41 +0200

    gen_bindings.sh

--- examples/basic_ibe/frontend/scripts/gen_bindings.sh
+++ examples/basic_ibe/frontend/scripts/gen_bindings.sh
@@ -2,9 +2,10 @@
 
 cd ../../backend && make extract-candid
 
-dfx generate basic_ibe || exit 1
+cd .. && dfx generate basic_ibe || exit 1
 
-rm -r ../frontend/src/declarations/basic_ibe > /dev/null 2>&1 || true
+rm -r frontend/src/declarations/basic_ibe > /dev/null 2>&1 || true
 
-mv ../src/declarations/basic_ibe ../frontend/src/declarations/basic_ibe
-rmdir -p ../src/declarations > /dev/null 2>&1 || true
+mkdir -p frontend/src/declarations/basic_ibe
+mv src/declarations/basic_ibe frontend/src/declarations
+rmdir -p src/declarations > /dev/null 2>&1 || true
