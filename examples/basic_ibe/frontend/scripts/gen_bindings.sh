5615b1cfb4d42a9a6a3dffa7cd15878d0ab9ff57
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-25 10:28:28 +0200

    add helper fns for VetKD calls, restructure basic_ibe

--- examples/basic_ibe/frontend/scripts/gen_bindings.sh
+++ examples/basic_ibe/frontend/scripts/gen_bindings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ../../backend && make extract-candid && dfx generate basic_ibe
+cd ../../backend && make extract-candid; dfx generate basic_ibe && rm -r ../frontend/src/declarations && mv ../src/declarations ../frontend/src/declarations && rmdir src
