6b63e01ab97fb673d0fe09c285dc9ab63050ede9
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-25 13:51:37 +0200

    II and init_arg

--- examples/basic_ibe/motoko/deploy_locally.sh
+++ examples/basic_ibe/motoko/deploy_locally.sh
@@ -8,9 +8,6 @@ dfx --version >> /dev/null
 # Run `dfx` if it is not already running.
 dfx ping &> /dev/null || dfx start --background --clean >> /dev/null
 
-# Deploy the Internet Identity canister.
-dfx deps pull && dfx deps init && dfx deps deploy
-
 # Deploy backend canister.
 dfx deploy --argument '("dfx_test_key")' basic_ibe
 
