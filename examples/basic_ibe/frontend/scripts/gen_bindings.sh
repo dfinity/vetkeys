1934c40b3ea1cb7a8f228bba5cc7268c8c6b447f
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-25 11:53:46 +0200

    finish restructuring (hopefully)

--- examples/basic_ibe/frontend/scripts/gen_bindings.sh
+++ examples/basic_ibe/frontend/scripts/gen_bindings.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-cd ../../backend && make extract-candid; dfx generate basic_ibe && rm -r ../frontend/src/declarations && mv ../src/declarations ../frontend/src/declarations && rmdir src
+cd ../../backend && make extract-candid
+
+dfx generate basic_ibe || exit 1
+
+rm -r ../frontend/src/declarations/basic_ibe > /dev/null 2>&1 || true
+
+mv ../src/declarations/basic_ibe ../frontend/src/declarations/basic_ibe
+rmdir -p ../src/declarations > /dev/null 2>&1 || true
