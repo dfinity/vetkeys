ab7ba7677c27b9aa50244ffb82b4184870bbee38
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-26 18:25:18 +0200

    fix copy-paste

--- examples/password_manager_with_metadata/frontend/scripts/gen_bindings.sh
+++ examples/password_manager_with_metadata/frontend/scripts/gen_bindings.sh
@@ -1,9 +1,9 @@
 cd ../../backend && make extract-candid
 
-cd .. && dfx generate basic_ibe || exit 1
+cd .. && dfx generate password_manager_with_metadata || exit 1
 
-rm -r frontend/src/declarations/basic_ibe > /dev/null 2>&1 || true
+rm -r frontend/src/declarations/password_manager_with_metadata > /dev/null 2>&1 || true
 
-mkdir -p frontend/src/declarations/basic_ibe
-mv src/declarations/basic_ibe frontend/src/declarations
+mkdir -p frontend/src/declarations/password_manager_with_metadata
+mv src/declarations/password_manager_with_metadata frontend/src/declarations
 rmdir -p src/declarations > /dev/null 2>&1 || true
