f1661800f8df7631ba287f0ab1b6e965ac012c26
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2025-06-27 10:01:07 +0200

    gen_bindings.sh

---
+++ examples/encrypted_notes_dapp_vetkd/frontend/scripts/gen_bindings.sh
@@ -0,0 +1,7 @@
+cd ../.. && dfx generate encrypted_notes || exit 1
+
+rm -r frontend/src/declarations/encrypted_notes > /dev/null 2>&1 || true
+
+mkdir -p frontend/src/declarations/encrypted_notes
+mv src/declarations/encrypted_notes frontend/src/declarations
+rmdir -p src/declarations > /dev/null 2>&1 || true
