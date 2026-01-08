6acc574e6e31f8556437e8883313393ddbc49349
Jack Lloyd <jack.lloyd@dfinity.org>
2026-01-08 16:29:06 -0500

    chore: Add a hint to use offline derivation in the examples
    It is kind of a pain to use offline derivation in the examples themselves,
    due to needing to reliably handle (and thus distinguish) if they are
    using PocketIC or mainnet. But we can leave a hint to readers that they
    might want to use offline derivation themselves.

--- examples/basic_bls_signing/rust/backend/src/lib.rs
+++ examples/basic_bls_signing/rust/backend/src/lib.rs
@@ -96,6 +96,10 @@ async fn get_my_verification_key() -> VetKeyPublicKey {
         context: context(&ic_cdk::api::msg_caller()),
         key_id: key_id(),
     };
+
+    // In production systems it would make sense to use the offline derivation
+    // support available in ic_vetkeys, eg DerivedPublicKey::derive_sub_key
+    // since this avoids the cycles and time overhead of a management canister call
     let result = ic_cdk::management_canister::vetkd_public_key(&request)
         .await
         .expect("call to vetkd_public_key failed");
