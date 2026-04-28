17a9d2e6902d944ed619f341cadcb4f6bc8fe15c
Marco Walz <marco.walz@dfinity.org>
2026-04-28 15:44:25 +0200

    fix: replace unsound Uint8Array cast in VetKey constructor with defensive copy

--- frontend/ic_vetkeys/src/utils/utils.ts
+++ frontend/ic_vetkeys/src/utils/utils.ts
@@ -556,7 +556,7 @@ export class VetKey {
     constructor(pt: G1Point) {
         this.#pt = pt;
 
-        this.#bytes = pt.toBytes(true) as Uint8Array<ArrayBuffer>;
+        this.#bytes = new Uint8Array(pt.toBytes(true));
     }
 }
 
