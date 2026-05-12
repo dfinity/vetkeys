8ff744818650d313d706c30782c52705a1917b70
Martin Raszyk <martin.raszyk@dfinity.org>
2026-05-12 07:49:22 +0000

    fix: flaky should_get_owned_map_names test

--- backend/rs/canisters/ic_vetkeys_encrypted_maps_canister/tests/tests.rs
+++ backend/rs/canisters/ic_vetkeys_encrypted_maps_canister/tests/tests.rs
@@ -918,7 +918,9 @@ fn should_get_owned_map_names() {
         }
 
         let map_name = random_map_name(rng);
-        expected_map_names.push(map_name.clone());
+        if !expected_map_names.contains(&map_name) {
+            expected_map_names.push(map_name.clone());
+        }
 
         for _ in 1..3 {
             let map_key = random_map_key(rng);
@@ -950,7 +952,7 @@ fn should_get_owned_map_names() {
                 encode_args((caller, map_name.clone())).unwrap(),
             )
             .unwrap();
-            expected_map_names.pop();
+            expected_map_names.retain(|n| n != &map_name);
         }
     }
 }
